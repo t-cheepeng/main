bcb5329b16c9170850661ecfe893c8a9212e37df
t-cheepeng <tcp97@hotmail.com>
2019-10-26 14:46:56 +0800

    Add IndexUtilTest

--- src/main/java/seedu/exercise/commons/core/index/IndexUtil.java
+++ src/main/java/seedu/exercise/commons/core/index/IndexUtil.java
@@ -1,5 +1,7 @@
 package seedu.exercise.commons.core.index;
 
+import static seedu.exercise.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.util.List;
 
 /**
@@ -8,6 +10,7 @@ import java.util.List;
 public class IndexUtil {
 
     public static <T> boolean isIndexOutOfBounds(Index index, List<T> list) {
+        requireAllNonNull(index, list);
         return index.getZeroBased() >= list.size();
     }
 
@@ -15,6 +18,7 @@ public class IndexUtil {
      * Checks if any of the indexes in {@code indexList} is out of bounds of the given {@code list}.
      */
     public static <T> boolean areIndexesOutOfBounds(List<Index> indexList, List<T> list) {
+        requireAllNonNull(indexList, list);
         for (Index index : indexList) {
             if (isIndexOutOfBounds(index, list)) {
                 return true;
